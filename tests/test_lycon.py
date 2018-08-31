2b5a06b61b39022a9fad9189eac4e202f6ef4b46
Alex Yang <zy5f9@mail.missouri.edu>
2018-08-31 17:58:17 -0500

    + test_lycon

---
+++ tests/test_lycon.py
@@ -0,0 +1,20 @@
+import os, sys
+import numpy as np
+from zutils.io import select
+
+ASSETS = lambda fname: os.path.join(os.path.dirname(__file__), 'assets', fname)
+
+def test_read_resize_lycon_shape():
+    read_resize = select('read_resize', backend='lycon')
+    im_test = read_resize(ASSETS('beach24.png'), (224,224))
+    assert im_test.shape == (224,224,3)
+
+def test_read_resize_lycon_dtype():
+    read_resize = select('read_resize', backend='lycon')
+    im_test = read_resize(ASSETS('beach24.png'), (224,224))
+    assert im_test.dtype == np.dtype('u1')
+
+def test_read_resize_lycon_range():
+    read_resize = select('read_resize', backend='lycon')
+    im_test = read_resize(ASSETS('beach24.png'), (224,224))
+    assert 20 < im_test.mean() < 220
